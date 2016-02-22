90d8a3e5c4128f12a31ed19f1983fe8739078e55
Christopher Jones <cejone08@louisville.edu>
2016-02-22 10:26:08 -0500

    2

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angular2App
+ */
+angular.module('angular2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angular2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angular2App
+ */
+angular.module('angular2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
